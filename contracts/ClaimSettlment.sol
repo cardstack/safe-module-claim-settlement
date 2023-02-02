392973c925bc9141023a40bab77f4ea65eaa6bd6
JT <justinthong93@gmail.com>
2023-02-02 23:29:43 +0800

    Copy spike code over (#1)
    * Copy
    
    * update package.json
    
    * remove consensus signed
    
    * fix ci
    
    * remove import
    
    * remove merkle module
    
    * didn need test
    
    * remove hardha console log
    
    * remove IConfig
    
    * remove config
    
    * rename files
    
    * rename module in test and alll over
    
    * reformat module
    
    remove logs
    
    cleanup module
    
    * fix test
    
    fix test
    
    * remove unused test contracts
    
    * remove .DS_Store file
    
    * update license

---
+++ contracts/ClaimSettlment.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/interfaces/IERC721.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@gnosis.pm/zodiac/contracts/core/Module.sol";
+import "./ClaimSettlementBase.sol";
+
+contract ClaimSettlement is ClaimSettlementBase {
+    address public keySigner;
+
+    constructor(address _owner, address _avatar, address _target) {
+        bytes memory initParams = abi.encode(_owner, _avatar, _target);
+        setUp(initParams);
+    }
+
+    function setUp(bytes memory initParams) public override initializer {
+        (address _owner, address _avatar, address _target) = abi.decode(
+            initParams,
+            (address, address, address)
+        );
+        __Ownable_init();
+        require(_avatar != address(0), "Avatar can not be zero address");
+        require(_target != address(0), "Target can not be zero address");
+        keySigner = _owner;
+        avatar = _avatar;
+        target = _target;
+
+        transferOwnership(_owner);
+    }
+
+    function isSigned(
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        bytes32 hash
+    ) public view returns (bool) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, hash));
+        address signer = ecrecover(prefixedHashMessage, v, r, s);
+        return signer == keySigner;
+    }
+
+    function signedExecute(
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        bytes memory leaf,
+        bytes memory extraData
+    ) public {
+        // verify in tree
+        (
+            address module,
+            bytes4 formats,
+            bytes memory validityData,
+            bytes memory userData,
+            bytes memory actionData
+        ) = abi.decode(leaf, (address, bytes4, bytes, bytes, bytes));
+        require(module == address(this), "invalid module");
+        bytes32 leafHash = keccak256(leaf);
+        require(formats[0] == hex"01", "invalid format");
+        // Check it's signed
+        require(isSigned(v, r, s, leafHash), "invalid signature");
+        // Check valid time/etc
+        require(isValid(formats[1], validityData, leafHash), "invalid leaf");
+        // Check user is allowed to call this
+        require(isValidUser(formats[2], userData), "invalid caller");
+        // Execute action
+        require(
+            executeAction(formats[3], actionData, extraData),
+            "action failed"
+        );
+        // Mark as used, if applicable
+        if (formats[1] == hex"01") {
+            used[leafHash] = true;
+        }
+        // emit event
+    }
+}
---
+++ test/utils.ts
@@ -0,0 +1,41 @@
+import { ethers } from "hardhat";
+import { utils } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { TransactionReceipt } from "@ethersproject/providers";
+
+export const getNativeBalance = async (address: string) => {
+  return ethers.provider.getBalance(address);
+};
+
+export const signMessage = async (
+  message: string,
+  signer: SignerWithAddress
+) => {
+  const abiCoder = new ethers.utils.AbiCoder();
+  const messageHashBytes = utils.arrayify(utils.keccak256(message));
+  const signature = await signer.signMessage(messageHashBytes);
+  const r = signature.slice(0, 66);
+  const s = "0x" + signature.slice(66, 130);
+  const v = parseInt(signature.slice(130, 132), 16);
+  const encodedSignature = abiCoder.encode(
+    ["uint8", "bytes32", "bytes32"],
+    [v, r, s]
+  );
+  // const o = await utils.verifyMessage(messageHashBytes, signature);
+  return {
+    signature,
+    encodedSignature,
+    r,
+    s,
+    v,
+    messageHashBytes,
+  };
+};
+
+export const gasUsedByTx = (receipt: TransactionReceipt) => {
+  return receipt.cumulativeGasUsed.mul(receipt.effectiveGasPrice);
+};
+
+export const getMethodId = (functionSignature: string) => {
+  return utils.id(functionSignature).substring(0, 10);
+};
